3ac19bdb6c57d1833eb3a8c8fc72da9a5c9fb444
Abanoub9518 <abanoub.romany9518@gmail.com>
2024-07-22 16:38:50 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { RegistrationFormComponent } from "./components/registration-form/registration-form.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: RegistrationFormComponent,
+  },
+  {
+    path: "register",
+    component: RegistrationFormComponent,
+  },
+];
